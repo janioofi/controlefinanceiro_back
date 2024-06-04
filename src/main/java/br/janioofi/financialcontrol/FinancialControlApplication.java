aea4d262bd67769e2623531d8eb0dc66e9417a18
Jânio <janioofi@gmail.com>
2024-06-04 19:30:16 -0300

    first commit

---
+++ src/main/java/br/janioofi/financialcontrol/FinancialControlApplication.java
@@ -0,0 +1,13 @@
+package br.janioofi.financialcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialControlApplication.class, args);
+    }
+
+}
