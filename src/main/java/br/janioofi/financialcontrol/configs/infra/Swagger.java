ad40f5df516d8fe9f99f219a8b1b60cd144effcc
Jânio <janioofi@gmail.com>
2024-06-04 20:46:26 -0300

    swagger configuration

---
+++ src/main/java/br/janioofi/financialcontrol/configs/infra/Swagger.java
@@ -0,0 +1,36 @@
+package br.janioofi.financialcontrol.configs.infra;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+
+    @Bean
+    public GroupedOpenApi publicApi(){
+        return GroupedOpenApi.builder()
+                .group("financial-control")
+                .displayName("Financial Control API")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Financial Control - API")
+                        .description("Rest API of a financial control system")
+                        .contact(new Contact()
+                                .name("@janioofi")
+                                .email("janioofi@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://financialcontrol.com.br/api/licenca")));
+    }
+}
